427165c894343213a89c77f411cf3a7cb232acd4
double1010x2 <double1010x2@gmail.com>
2020-05-01 00:26:09 +0800

    [Day-008] learning string homework:     julia_008_hw.jl

---
+++ homework/day008/julia_008_hw.jl
@@ -0,0 +1,25 @@
+# %%
+#'''
+# Julia 字元 (Char) 與字串 (String)
+
+## Day 008 作業：讀取使用者輸入字串，並印出每個字元
+
+#1. 使用 `readline()` 函式，讀取使用者輸入之字串。`readline()`是從 console 讀取使用者輸入的方式，讀進來的資料型別為字串，`readline()`的說明可參照官方文件 [Base.readline](https://docs.julialang.org/en/v1/base/io-network/#Base.readline)
+#'''
+#
+# %%
+str = readline() 
+
+for si in str
+    println(si)
+end
+# %%
+#'''
+#2. 印出字串中所有字元。
+#'''
+
+# %%
+# 程式碼
+
+
+# %%

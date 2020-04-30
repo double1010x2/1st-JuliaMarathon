6d4eac5c8f6e08b813daffc276e691dad9ab0b3e
double1010x2 <double1010x2@gmail.com>
2020-05-01 01:18:09 +0800

    [Day-009] learning array and sort algorithm homework:     julia_009_hw.jl results:     speed:         QucikSort > InsertionSort > PartialQuickSort > MergeSort     memory usuage:         MergeSort > PArtialQuickSort > InsertionSort > QuickSort     [Insertion Sort]         0.028324 seconds (74.41 k allocations: 4.815 MiB)     [Merge Sort]         0.051651 seconds (138.00 k allocations: 7.850 MiB)     [Partial Quick Sort]         0.040365 seconds (98.68 k allocations: 5.964 MiB)     [Quick Sort]         0.008700 seconds (25.04 k allocations: 2.132 MiB)

---
+++ homework/day009/julia_009_hw.jl
@@ -0,0 +1,53 @@
+# %%
+#'''
+# 陣列 (Array)
+
+## Day 009 作業：比較不同的內建排序演算法
+
+#除了範例程式示範的 QuickSort 之外，Julia 也內建支援幾種不同的排序演算法：
+#
+#- InsertionSort
+#- PartialQuickSort(k)
+#- MergeSort
+#
+#1. 請閱讀官方文件及範例 [Sorting-Algorithms](https://docs.julialang.org/en/v1/base/sort/#Sorting-Algorithms-1)
+#
+#2. 撰寫程式隨機產生 100000 個 200 到 500 之間的數字來建立陣列，執行不同排序方法，來比較不同排序方法的速度。
+
+#**[提示]** 排序執行所需時間可以用巨集 `@time` 或 `@elapsed` 來取得。
+#'''
+
+# %%
+# 產生 100000 個 200 到 500 之間的數字的陣列
+x = rand(200:500, 100000) 
+
+## Insertion Sort
+println("[Insertion Sort]")
+@time is = sort(x; alg=InsertionSort) 
+
+# %%
+
+
+## Merge Sort
+println("[Merge Sort]")
+@time is = sort(x; alg=MergeSort) 
+
+# %%
+
+
+## Partial Quick Sort
+println("[Partial Quick Sort]")
+@time is = sort(x; alg=PartialQuickSort(10000)) 
+
+# %%
+
+
+## QuickSort
+println("[Quick Sort]")
+@time is = sort(x; alg=QuickSort) 
+
+# %%
+#@elapsed sort(x; alg=QuickSort)
+
+## 請問哪一種排序方法的效能最好？
+

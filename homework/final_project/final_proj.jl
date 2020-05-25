166fb4cd2404eab68ca027219897722443d653b9
double1010x2 <double1010x2@gmail.com>
2020-05-26 00:26:13 +0800

    [Day034 ~ 036] revised

--- homework/final_project/final_proj.jl
+++ homework/final_project/final_proj.jl
@@ -29,10 +29,10 @@ image_path              = Array([image_training, image_verify])
 
 #========== training parameter ===========#
 n_class     = 10
-batchsize   = 16
+batchsize   = [8, 32, 64]
 batchsize2  = 64
 learn_rate  = 0.005
-epochs      = 10
+epochs      = [10, 10, 10]
 epochs2     = 10
 #========== funciton ===========#
 
@@ -150,74 +150,74 @@ test_x      = image_dict["verify"]
 test_y      = image_dict["verify_tar"]
 train_y     = onehotbatch(train_y, 0:9)
 test_y      = onehotbatch(test_y, 0:9)
-train       = DataLoader(train_x, train_y, batchsize=batchsize, shuffle=true)
-test        = DataLoader(test_x, test_y, batchsize=batchsize)
+#train       = DataLoader(train_x, train_y, batchsize=batchsize, shuffle=true)
+#test        = DataLoader(test_x, test_y, batchsize=batchsize)
 
 #model       = vgg16()
 
-#layer_arr   = []
-##========== level 1 (down to 150x150)==========#
-#filters     = (3, 16)
-#sstep       = 2
-#step_b      = 1
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 2 (down to 75x75)==========#
-#filters     = (16, 32)
-#sstep       = 2
-#step_b      = 2
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 3 (down to 38x38)==========#
-#filters     = (32, 32)
-#sstep       = 2
-#step_b      = 2
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 4 (down to 19x19) ==========#
-#filters     = (32, 64)
-#sstep       = 2
-#step_b      = 4
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 4 (down to 10x10) ==========#
-#filters     = (64, 128)
-#sstep       = 2
-#step_b      = 16
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 5 (down to 5x5) ==========#
-#filters     = (128, 256)
-#sstep       = 2
-#step_b      = 32
-#shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
-#push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
-#push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
-##========== level 6 (MeanPooling to 19x19) ==========#
-#push!(layer_arr, MeanPool((5,5)))
-#
-#push!(layer_arr, x->reshape(x, :, size(x,4)))
-#push!(layer_arr, (Dense(256, 10)))
-#push!(layer_arr, softmax)
-#model       = Chain(layer_arr...)
-
-layer_arr    = []
-push!(layer_arr, Conv2d_bn(3,  8,  (3,3),  (1,1), (2,2), leakyrelu))  #150x150
-push!(layer_arr, Conv2d_bn(8,  16, (3,3),  (1,1), (2,2), leakyrelu))  #75x75
-push!(layer_arr, Conv2d_bn(16, 32, (3,3),  (1,1), (2,2), leakyrelu))  #38x38
-push!(layer_arr, Conv2d_bn(32, 64, (3,3),  (1,1), (2,2), leakyrelu))  #19x19
-push!(layer_arr, Conv2d_bn(64, 128, (3,3), (1,1), (2,2), leakyrelu)) #10x10
-push!(layer_arr, MaxPool((3,3))) 
+layer_arr   = []
+#========== level 1 (down to 150x150)==========#
+filters     = (3, 16)
+sstep       = 2
+step_b      = 1
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 2 (down to 75x75)==========#
+filters     = (16, 32)
+sstep       = 2
+step_b      = 2
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 3 (down to 38x38)==========#
+filters     = (32, 32)
+sstep       = 2
+step_b      = 2
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 4 (down to 19x19) ==========#
+filters     = (32, 64)
+sstep       = 2
+step_b      = 4
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 4 (down to 10x10) ==========#
+filters     = (64, 128)
+sstep       = 2
+step_b      = 16
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 5 (down to 5x5) ==========#
+filters     = (128, 256)
+sstep       = 2
+step_b      = 32
+shortcut0   = Chain(Conv((1,1), filters[1]=>filters[2], pad = (0,0), stride = (sstep,sstep)), BatchNorm(filters[2]))
+push!(layer_arr, Metalhead.ResidualBlock([filters[1], step_b, step_b, filters[2]], [1,3,1], [0,1,0], [1,1,sstep], shortcut0))
+push!(layer_arr, Conv((1,1), filters[2]=>filters[2], leakyrelu, pad=(0,0), stride=(1,1)))
+#========== level 6 (MeanPooling to 19x19) ==========#
+push!(layer_arr, MeanPool((5,5)))
 
 push!(layer_arr, x->reshape(x, :, size(x,4)))
-push!(layer_arr, (Dense(1152, 10)))
+push!(layer_arr, (Dense(256, 10)))
 push!(layer_arr, softmax)
 model       = Chain(layer_arr...)
+
+#layer_arr    = []
+#push!(layer_arr, Conv2d_bn(3,  8,  (3,3),  (1,1), (2,2), leakyrelu))  #150x150
+#push!(layer_arr, Conv2d_bn(8,  16, (3,3),  (1,1), (2,2), leakyrelu))  #75x75
+#push!(layer_arr, Conv2d_bn(16, 32, (3,3),  (1,1), (2,2), leakyrelu))  #38x38
+#push!(layer_arr, Conv2d_bn(32, 64, (3,3),  (1,1), (2,2), leakyrelu))  #19x19
+#push!(layer_arr, Conv2d_bn(64, 128, (3,3), (1,1), (2,2), leakyrelu)) #10x10
+#push!(layer_arr, MaxPool((3,3))) 
+#
+#push!(layer_arr, x->reshape(x, :, size(x,4)))
+#push!(layer_arr, (Dense(1152, 10)))
+#push!(layer_arr, softmax)
+#model       = Chain(layer_arr...)
 model   = model     |> cpu
 train_x = train_x   |> cpu
 train_y = train_y   |> cpu
@@ -229,22 +229,19 @@ loss(x, y) = logitcrossentropy(model(x), y)
 
 evalcb() = @show(test_loss())
 
-println("[Training Start !!!]")
-println("\tTraining size: $(size(train_x)[end])")
-println("\tTest     size: $(size(test_x)[end])")
-println("\tBatch    size: $(batchsize)")
-println("\tThreads count: $(Threads.nthreads())")
-@epochs epochs Flux.train!(loss, params(model), train, ADAM(learn_rate), cb=throttle(evalcb, 10))
-
-train       = DataLoader(train_x, train_y, batchsize=batchsize2, shuffle=true)
-test        = DataLoader(test_x, test_y, batchsize=batchsize2)
-println("[Training Start 2!!!]")
-println("\tTraining size: $(size(train_x)[end])")
-println("\tTest     size: $(size(test_x)[end])")
-println("\tBatch    size: $(batchsize2)")
-println("\tThreads count: $(Threads.nthreads())")
-@epochs epochs2 Flux.train!(loss, params(model), train, ADAM(learn_rate), cb=throttle(evalcb, 10))
-
+train       = DataLoader(train_x, train_y, batchsize=batchsize[1], shuffle=true)
+test        = DataLoader(test_x, test_y, batchsize=batchsize[1])
+for ii in 1:length(epochs)
+    global train       = DataLoader(train_x, train_y, batchsize=batchsize[ii], shuffle=true)
+    global test        = DataLoader(test_x, test_y, batchsize=batchsize[ii])
+    println("[Training Start !!!]")
+    println("\tTraining size: $(size(train_x)[end])")
+    println("\tTest     size: $(size(test_x)[end])")
+    println("\tBatch    size: $(batchsize[ii])")
+    println("\tThreads count: $(Threads.nthreads())")
+    @epochs epochs[ii] Flux.train!(loss, params(model), train, ADAM(learn_rate), cb=throttle(evalcb, 10))
+    accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
+end
 accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
 
 println(accuracy(test_x, test_y))
